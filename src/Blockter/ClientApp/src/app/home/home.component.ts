a8410db3ed5c286827ceb8e67b7808ea7acae166
Daniel Ho <webdevhod@gmail.com>
2023-10-21 01:30:02 -0700

    Added primeng.

--- src/Blockter/ClientApp/src/app/home/home.component.ts
+++ src/Blockter/ClientApp/src/app/home/home.component.ts
@@ -5,18 +5,43 @@ import { takeUntil } from "rxjs/operators";
 
 import { AccountService } from "app/core/auth/account.service";
 import { Account } from "app/core/auth/account.model";
-
+import { MenuItem } from "primeng/api";
+interface City {
+  name: string;
+  code: string;
+}
 @Component({
   selector: "jhi-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  cities: City[];
+  selectedCity: City;
+  items: MenuItem[];
+
   account: Account | null = null;
 
   private readonly destroy$ = new Subject<void>();
 
-  constructor(private accountService: AccountService, private router: Router) {}
+
+  constructor(private accountService: AccountService, private router: Router) {
+    this.cities = [
+      { name: "New York", code: "NY" },
+      { name: "Rome", code: "RM" },
+      { name: "London", code: "LDN" },
+      { name: "Istanbul", code: "IST" },
+      { name: "Paris", code: "PRS" },
+    ];
+    this.selectedCity = this.cities[0];
+    this.items = [
+      {label: 'Home', icon: 'pi pi-fw pi-home'},
+      {label: 'Calendar', icon: 'pi pi-fw pi-calendar'},
+      {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
+      {label: 'Documentation', icon: 'pi pi-fw pi-file'},
+      {label: 'Settings', icon: 'pi pi-fw pi-cog'}
+    ];
+  }
 
   ngOnInit(): void {
     this.accountService
--- src/Blockter/ClientApp/src/app/home/home.module.ts
+++ src/Blockter/ClientApp/src/app/home/home.module.ts
@@ -4,9 +4,13 @@ import { RouterModule } from "@angular/router";
 import { SharedModule } from "app/shared/shared.module";
 import { HOME_ROUTE } from "./home.route";
 import { HomeComponent } from "./home.component";
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DropdownModule } from 'primeng/dropdown';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), BrowserModule, BrowserAnimationsModule, DropdownModule, TabMenuModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
